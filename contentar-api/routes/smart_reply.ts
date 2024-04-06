17afb6ad47dd5f8c48659aad5a8de36f69a9f2d6
Byron Wang <openzig@gmail.com>
2024-04-06 09:22:11 -0700

    change to humorous style

--- contentar-api/routes/smart_reply.ts
+++ contentar-api/routes/smart_reply.ts
@@ -5,19 +5,18 @@ import OpenAI from "openai";
 const router = Router();
 
 const openai = new OpenAI({
-    apiKey: "",
+    apiKey: "API_PUBLIC_KEY",
 });
 
 router.route("/chat").post(async (req: Request, res: Response) => {
     let chat_messages: Array<OpenAI.Chat.ChatCompletionMessageParam> = [{
         role: "system",
         content: "You are a social media creator, and your job is to reply user's comments to your article." + 
-                 "Please keep the response as short as possible. Also, The response should be as mean as possible." +
+                 "Please keep the response as short as possible. Also, The response should be as humorous as possible." +
                  "Please use Chinese"
     }];
 
     const comments_chain: Array<OpenAI.Chat.ChatCompletionMessageParam> = req.body.messages;
-    console.log(comments_chain)
     chat_messages = chat_messages.concat(comments_chain);
     const params: OpenAI.Chat.ChatCompletionCreateParams = {
         messages: chat_messages,
@@ -28,9 +27,7 @@ router.route("/chat").post(async (req: Request, res: Response) => {
         frequency_penalty: 0,
         presence_penalty: 0,
     };
-    console.log(params)
     const response: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
-    console.log(response)
     res.json(response.choices[0].message);
 });
 
